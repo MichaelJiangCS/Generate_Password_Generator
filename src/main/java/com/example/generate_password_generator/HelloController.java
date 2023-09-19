2a8a2d88bf7bfd5d54318d6beba64e2d618c09d6
MichaelJiangCS <97463680+MichaelJiangCS@users.noreply.github.com>
2023-09-19 19:43:22 -0400

    set title

--- src/main/java/com/example/generate_password_generator/HelloController.java
+++ src/main/java/com/example/generate_password_generator/HelloController.java
@@ -2,13 +2,12 @@ package com.example.generate_password_generator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private AnchorPane titleText;
+
+
 }
