be7bfb15101e03f623eeec4fafdd239f0e170960
zee <57326432+babayaga88@users.noreply.github.com>
2023-11-17 10:55:51 -0500

    dfsfs

--- src/main/java/com/example/generate_password_generator/HelloController.java
+++ src/main/java/com/example/generate_password_generator/HelloController.java
@@ -3,7 +3,10 @@ package com.example.generate_password_generator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
@@ -13,41 +16,61 @@ public class HelloController {
 
     @FXML
     private AnchorPane titleText;
-
     @FXML
     private TextArea passwordOutput;
     @FXML
     private TextArea lengthInput;
     @FXML
+    private TextArea secretWord;
+
+    // Buttons
+    @FXML
     private Button generateButton;
     @FXML
-    private ToggleButton uppercaseToggle;
+    private Button resetButton;
+
+    // Slider
     @FXML
-    private ToggleButton lowercaseToggle;
+    private ChoiceBox<String> wordChoicePlacement;
+
+    // ToggleButtons
     @FXML
-    private ToggleButton symbolsToggle;
+    private CheckBox uppercaseToggle;
     @FXML
-    private ToggleButton numbersToggle;
+    private CheckBox lowercaseToggle;
+    @FXML
+    private CheckBox symbolsToggle;
+    @FXML
+    private CheckBox numbersToggle;
 
 
     // not allowing user to edit the output textArea
     public void initialize(){
         passwordOutput.setEditable(false);
+        secretWord.setText("");
+        wordChoicePlacement.getItems().addAll("Beginning", "Middle", "End");
+        wordChoicePlacement.setValue("");
     }
 
     @FXML
     public void generateButtonClicked(ActionEvent actionEvent) {
-        // detect if any of the toggle is selected and store it as a boolean
+        // call generatePassword() method when this button is clicked
         boolean anyToggleSelected = uppercaseToggle.isSelected() || lowercaseToggle.isSelected() || numbersToggle.isSelected()
                 || symbolsToggle.isSelected();
 
-        // convert text into an int
+        // convert user input into an int
         int passwordLength = Integer.parseInt(lengthInput.getText());
 
+        // convert user word input to a String
+        String SecretWordInput = (secretWord.getText().equals("")) ? "" : secretWord.getText();
+
+        
+
+
         // length must be greater than 0
-        if(passwordLength <= 0) {
-            // use textArea to display an error msg
-            passwordOutput.setText("Error: only positive integers are accepted!");
+        if(passwordLength <= 0){
+            // disable Generate button
+            generateButton.setDisable(true);
             return;
         }
 
@@ -57,11 +80,25 @@ public class HelloController {
                     uppercaseToggle.isSelected(),
                     lowercaseToggle.isSelected(),
                     numbersToggle.isSelected(),
-                    symbolsToggle.isSelected());
+                    symbolsToggle.isSelected(),
+                    SecretWordInput,
+                    wordChoicePlacement.getValue());
 
             // output generated password
             passwordOutput.setText(generatedPassword);
         }
 
+    }
+
+    @FXML
+    public void resetButtonClick(ActionEvent actionEvent){
+        lengthInput.setText("");
+        passwordOutput.setText("");
+        secretWord.setText("");
+        uppercaseToggle.setSelected(false);
+        lowercaseToggle.setSelected(false);
+        numbersToggle.setSelected(false);
+        symbolsToggle.setSelected(false);
+        symbolsToggle.setSelected(false);
     }
 }
